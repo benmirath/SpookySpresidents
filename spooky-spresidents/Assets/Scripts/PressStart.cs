94dc988f237747dff1935ebd323b0c8b5f628848
Fabian Rastorfer <fabraz@mac.com>
2014-11-01 20:00:51 -0400

    Ttitle Spleen Yo!

---
+++ spooky-spresidents/Assets/Scripts/PressStart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressStart : MonoBehaviour {
+	public void PressStartFunction () {
+		Application.LoadLevel("Tileset Chunks");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
