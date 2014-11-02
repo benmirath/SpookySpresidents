e05647770cc5ac1f0ddd314cb85cf669caf5220b
DynamicDonut <bwilldonut@gmail.com>
2014-11-02 17:16:11 -0500

    Deleting Met

--- spooky-spresidents/Assets/Scripts/CameraFollowScript.cs
+++ spooky-spresidents/Assets/Scripts/CameraFollowScript.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CameraFollowScript : MonoBehaviour {
 	public GameObject target;
-	public float bg_flip;
+	private GameObject[] enemy;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		bg_flip = this.transform.FindChild ("Background").transform.position.z;
+		enemy = GameObject.FindGameObjectsWithTag ("Enemy");
+		/*bg_flip = this.transform.FindChild ("Background").transform.position.z;
 		if (!target.GetComponent<CharacterControllerScript> ().facingRight) {
 			bg_flip *= -1;
 		}
-		Debug.Log (bg_flip);
+		Debug.Log (bg_flip);*/
+		Debug.Log (enemy.Length);
 	}
 }
