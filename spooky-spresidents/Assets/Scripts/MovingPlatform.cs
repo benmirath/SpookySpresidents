01117faff3f231a40cca6b3361bd5219c66f8502
Ben Miller <bjmillers@gmail.com>
2014-11-02 14:57:20 -0500

    Adding in moving platforms

---
+++ spooky-spresidents/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+	bool right = true;
+	public Transform limiterRight;
+	public Transform limiterLeft;
+	public float idleTimer;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Movement ());
+	}
+	IEnumerator Movement () {
+		while (true) {
+			int dir;
+			if (right) {
+				if (transform.position.x < limiterRight.position.x) {
+					//walk right
+					dir = 1;
+				} else {
+					dir = 0;
+					right = false;
+					rigidbody2D.velocity = Vector2.zero;
+					yield return new WaitForSeconds (idleTimer);
+				}
+			} else {
+				if (transform.position.x > limiterLeft.position.x) {
+					//walk right
+				    dir = -1;
+				} else {
+					dir = 0;
+					right = true;
+					rigidbody2D.velocity = Vector2.zero;
+					yield return new WaitForSeconds (idleTimer);
+				}
+			}
+			rigidbody2D.velocity = new Vector2 (dir * speed, rigidbody2D.velocity.y);
+			yield return null;		
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- spooky-spresidents/Assets/Scripts/Treadmill.cs
+++ spooky-spresidents/Assets/Scripts/Treadmill.cs
@@ -20,14 +20,10 @@ public class Treadmill : MonoBehaviour {
 	}
 	void OnTriggerStay2D (Collider2D collider) {
 		float dir = 0;
-
-		Debug.Log ("BOOST");
-
 		if (right) dir = 1;
 		else dir = -1;
 
 		if (collider.tag == "Player" || collider.tag == "Enemy") {
-			Debug.Log ("WOOT");
 			collider.rigidbody2D.AddForce (new Vector2(dir * speed, 0));
 		}
 	}
